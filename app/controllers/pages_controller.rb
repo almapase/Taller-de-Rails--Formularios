a8c981cd59683dfbc6b4672c793e0da8622d8127
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-05 00:49:40 -0500

    mensaje registrado

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,13 @@ class PagesController < ApplicationController
   end
 
   def landing
+    @alerta = ""
     render layout: "layout_landing"
   end
 
   def contact
     @user= User.create(name: params[:name], email: params[:email], edad: params[:age])
-    redirect_to root_path, notice: "Garcias #{@user.name}, estas registrado"
+    @alerta= "alert alert-success"
+    redirect_to pages_landing_path, notice: "Garcias #{@user.name}, estas registrado"
   end
 end
