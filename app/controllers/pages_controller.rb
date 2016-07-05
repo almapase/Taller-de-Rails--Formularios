274ac3152625126ce43d0ce1ba7737fb86a606d7
Alex Pacheco <alex.pacheco.s@gmail.com>
2016-07-04 19:48:56 -0500

    Hacer el método en el controller para procesar el formulario y guardar los datos en el modelo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def contact
+    @user= User.create(name: params[:name], email: params[:email], age: params[:age])
+    redirect_to root_path, notice: "Garcias #{@user.name}, estas registrado"
+  end
 end
